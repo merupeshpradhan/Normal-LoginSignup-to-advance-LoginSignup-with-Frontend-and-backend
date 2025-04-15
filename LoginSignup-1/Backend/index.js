e86634fec2895ea68a48b64284b2004c72459590
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-04-15 22:20:26 +0530

    Add module type and dependencies to package.json for backend setup

--- LoginSignup-1/Backend/index.js
+++ LoginSignup-1/Backend/index.js
@@ -1,1 +1,15 @@
-console.log("Rupesh Backend Practice");
+import express from "express";
+
+import dotenv from "dotenv";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
+});
