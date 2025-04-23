71cafb484210ee0e63db1e4f77756c586cebdf61
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-04-23 22:47:09 +0530

    Frontend Litle thing added now

---
+++ Normal Login-Signup/Frontend/src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div className="bg-amber-400 min-h-[100vh]  text-white text-5xl font-semibold flex justify-center items-center">
+      <p>Homepage</p>
+    </div>
+  );
+}
+
+export default Homepage;
