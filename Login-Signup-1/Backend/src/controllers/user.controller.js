939d20a48b4cf8568d2671292cfdbb88c494bd14
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-05-06 22:00:47 +0530

    Implement user registration functionality with error handling and response structure

--- Login-Signup-1/Backend/src/controllers/user.controller.js
+++ Login-Signup-1/Backend/src/controllers/user.controller.js
@@ -1,4 +1,34 @@
-const registerUser = async (req, res) => {};
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import asyncHandler from "../utils/asyncHandler";
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName || !email || !password) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existedUser = await User.findOne({ email });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with this email already exists");
+  }
+
+  const user = await User.create({ fullName, email, password });
+
+  const userData = {
+    id: user._id,
+    fullName: user.fullName,
+    email: user.email,
+  };
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, userData, "User registered successfully"));
+});
+
 const loginUser = async (req, res) => {};
 const logoutUser = async (req, res) => {};
 
