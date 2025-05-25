8b17b52e064f92857c78235793388e9dec78d20b
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-05-25 08:55:40 +0530

    feat: add user login and logout functionality to user controller and routes that now my backend is complete for login-signup-4

--- Login-Signup-4/Backend/src/controllers/user.controller.js
+++ Login-Signup-4/Backend/src/controllers/user.controller.js
@@ -59,4 +59,43 @@ const userRegister = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, userData, "You registered successfully"));
 });
 
-export { userRegister };
+const userLogin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400, "Please enter correct email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(400, "This email is not here");
+  }
+
+  const isPasswordisCorrect = user.password === password;
+
+  if (!isPasswordisCorrect) {
+    throw new ApiError(400, "Please provide correct password");
+  }
+
+  const userData = {
+    id: user._id,
+    fullName: user.fullName,
+    fatherName: user.fatherName,
+    motherName: user.motherName,
+    phoneNumber: user.phoneNumber,
+    email: user.email,
+  };
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userData, "User login successfuly"));
+});
+
+const userLogout = asyncHandler(async (req, res) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "User logout Successfuly"));
+});
+
+export { userRegister, userLogin, userLogout };
