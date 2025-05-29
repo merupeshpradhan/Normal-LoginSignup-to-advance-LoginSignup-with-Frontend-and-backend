99c5743540dcf66c1749cc15e48903bb89bac836
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-05-29 15:17:26 +0530

    feat: implement user model with password hashing and comparison methods

---
+++ Login-Signup-6/Backend/src/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Please enter your full name"],
+    },
+    DOB: {
+      type: Date,
+      required: [true, "Please enter your date of birth"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter for your password"],
+    },
+  },
+  { timeStamps: true }
+);
+
+// HASSING IN PASSWORD
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// Compare password
+userSchema.methods.isPasswordCompare = async function (password) {
+  return await bcrypt.compare(password, this.passwordX);
+};
+
+export const User = mongoose.model("User", userSchema);
