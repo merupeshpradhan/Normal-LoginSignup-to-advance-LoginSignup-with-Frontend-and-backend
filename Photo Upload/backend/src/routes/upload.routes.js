cda46ca636f7944280d34d705efbc030e9bde6f0
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-06-12 18:38:07 +0530

    THE PHOTO UPLOAD IN CLOUUDINARY SETUP NOW WORK AND ITS NOW ITS COMPLETE

--- Photo Upload/backend/src/routes/upload.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { upload } from "../utils/upload.js";
-import { handleFileUpload } from "../controllers/upload.controller.js";
-
-const router = express.Router();
-
-router.post("/upload", upload.single("photo"), handleFileUpload);
-
-export default router;
