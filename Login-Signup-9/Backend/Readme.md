981a1b8c7a2ca62aded0063115d583718efde87a
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-06-14 09:25:19 +0530

    litle file only file setup chnage

---
+++ Login-Signup-9/Backend/Readme.md
@@ -0,0 +1,16 @@
+# Login-Signup-7
+
+### Remove item frome the git using this write in terminal
+
+```
+git rm --cached .env
+git rm -r --cached node_modules
+```
+
+### Here one install cloudinary
+
+```
+(i) cloudinary -> npm i cloudinary
+(ii) multer -> npm i multer
+
+```
---
+++ Login-Signup-9/Backend/src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Here frintend connection
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Here Import routes
+import userRouter from "./routes/user.route.js";
+
+// Here Routes start for use
+app.use("/api/v1/users", userRouter);
+
+export { app };
