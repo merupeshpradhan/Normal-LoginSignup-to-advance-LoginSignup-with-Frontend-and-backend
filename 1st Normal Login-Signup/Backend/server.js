5b9bc80c98071f4e49bef7444c900215b0e158f5
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-06-18 11:14:46 +0530

    Just change file names

--- 1st Normal Login-Signup/Backend/server.js
+++
@@ -1,5 +0,0 @@
-import app from "./app.js";
-
-app.listen(process.env.PORT,()=>{
-    console.log(`\n Server running on port ${process.env.PORT}`);
-})
--- Login-Signup-4/Backend/Readme.md
+++
@@ -1,8 +0,0 @@
-# Login-Signup-4
-
-### Now here install one mor thing
-
-```
-(i) bcrypt :- npm i bcrypt
-
-```
