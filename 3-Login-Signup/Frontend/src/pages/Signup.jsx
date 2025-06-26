c4b064008e8639bdabea0375d4b4c5a891d835ec
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-06-26 18:09:48 +0530

    Add axios for API calls and implement signup functionality in Signup component

--- 3-Login-Signup/Frontend/src/pages/Signup.jsx
+++ 3-Login-Signup/Frontend/src/pages/Signup.jsx
@@ -1,4 +1,45 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Signup() {
+  const [fullName, setFullName] = useState("");
+  const [motherName, setMotherName] = useState("");
+  const [fatherName, setFatherName] = useState("");
+  const [DOB, setDOB] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = (e) => {
+    console.log({ fullName, motherName, fatherName, DOB, email, password });
+
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:4000/api/v1/register", {
+        fullName,
+        motherName,
+        fatherName,
+        DOB,
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log("Register Successfully", res);
+        alert("User registeres successfully!");
+
+        setFullName("");
+        setMotherName("");
+        setFatherName("");
+        setDOB("");
+        setEmail("");
+        setPassword("");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Please write all");
+      });
+  };
+
   return (
     <div className="h-[100vh] flex justify-center items-center">
       <div className="shadow-md rounded-2xl border border-red-400 w-[20vw] h-[55vh]">
@@ -10,6 +51,8 @@ function Signup() {
             placeholder="Full name"
             className="border outline-0 pl-3 rounded-md w-full py-2"
             type="text"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
           />
         </div>
         <div className="mt-3 px-3">
@@ -17,6 +60,8 @@ function Signup() {
             placeholder="Mother name"
             className="border outline-0 pl-3 rounded-md w-full py-2"
             type="text"
+            value={motherName}
+            onChange={(e) => setMotherName(e.target.value)}
           />
         </div>
         <div className="mt-3 px-3">
@@ -24,13 +69,17 @@ function Signup() {
             placeholder="Father name"
             className="border outline-0 pl-3 rounded-md w-full py-2"
             type="text"
+            value={fatherName}
+            onChange={(e) => setFatherName(e.target.value)}
           />
         </div>
         <div className="mt-3 px-3">
           <input
             placeholder="DOB"
             className="border outline-0 pl-3 rounded-md w-full py-2"
-            type="text"
+            type="date"
+            value={DOB}
+            onChange={(e) => setDOB(e.target.value)}
           />
         </div>
         <div className="mt-3 px-3">
@@ -38,17 +87,26 @@ function Signup() {
             placeholder="Email"
             className="border outline-0 pl-3 rounded-md w-full py-2"
             type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div className="mt-3 px-3">
           <input
             placeholder="Password"
             className="border outline-0 pl-3 rounded-md w-full py-2"
-            type="text"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="mt-5 px-3 flex justify-center">
-          <button className="bg-red-500 py-2 px-15 rounded-xl text-lg font-bold text-white hover:bg-red-600">Signup</button>
+          <button
+            className="bg-red-500 py-2 px-5 rounded-xl text-lg font-bold text-white hover:bg-red-600"
+            onClick={handleSignup}
+          >
+            Signup
+          </button>
         </div>
       </div>
     </div>
