baa595b02e3dde045a919875b3270b3bb30e9e7b
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-06-26 14:32:57 +0530

    Refactor Login and Signup components for improved layout and styling

--- 3-Login-Signup/Frontend/src/pages/Login.jsx
+++ 3-Login-Signup/Frontend/src/pages/Login.jsx
@@ -1,48 +1,38 @@
-import React from "react";
-import {Link} from "react-router-dom"
-
-function Login() {
+function login() {
   return (
-    <div className="font-bold flex justify-center w-full h-[100vh] bg-indigo-500">
-      <div className="text-white content-center">
-        <div className="w-[20vw] sm:w-[25vw] h-1/2 sm:h-[60%] border rounded-2xl border-indigo-300">
-          <div className="">
-            <p className="flex justify-center mt-2 text-2xl font-semibold">Login</p>
+    <div className="h-[100vh] flex justify-center items-center">
+      <div className="border border-green-400 w-[20vw] h-[45vh] rounded-2xl shadow">
+        <div className="flex justify-center py-2 ">
+          <h3 className="text-xl font-bold text-yellow-500 tracking-wider">Login</h3>
+        </div>
+        <div className="mt-8">
+          <div className="px-5">
+            <p className="mb-1 font-bold">Email</p>
+            <input
+              className="border rounded-md w-full h-9 pl-5 outline-0"
+              placeholder="Email"
+              type="email"
+            />
           </div>
-          <div className="mt-15">
-            <div className="flex justify-center">
-              <input
-                type="email"
-                className="border-1 px-5 py-1 w-[80%] rounded-sm"
-                placeholder="Email"
-              />
-            </div>
-            <div className="flex justify-center">
-              <input
-                type="password"
-                className="border-1 px-5 py-1 w-[80%] rounded-sm mt-8"
-                placeholder="Password"
-              />
-            </div>
-            <div className="flex justify-center">
-              <button className="w-25 h-8 mt-9 rounded-sm bg-blue-500">
-                Login
-              </button>
-            </div>
-            <div className="flex justify-center">
-              <hr className="mt-10 w-[98%]" />
-            </div>
-            <div className="">
-
-            <Link className="flex justify-center mt-7">
-              <button className="bg-red-500 hover:bg-red-400 w-[95%] rounded-sm p-3">Register</button>
-            </Link>
-            </div>
+          <div className="px-5">
+            <p className="mb-1 mt-2 font-bold">Password</p>
+            <input
+              className="border rounded-md w-full h-9 pl-5 outline-0"
+              placeholder="Password"
+              type="password"
+            />
           </div>
+          <div className="flex justify-center mt-7">
+            <button className="bg-green-500 py-2 px-10 font-bold text-white rounded-md hover:bg-green-600">Login</button>
+          </div>
+        </div>
+        <hr className="mt-6"/>
+        <div className="signup flex justify-center mt-6 px-1">
+          <button className="border w-full p-2 rounded-xl bg-red-500 hover:bg-red-600 tracking-widest text-white font-bold text-base">Signup</button>
         </div>
       </div>
     </div>
   );
 }
 
-export default Login;
+export default login;
--- 3-Login-Signup/Frontend/src/pages/Signup.jsx
+++ 3-Login-Signup/Frontend/src/pages/Signup.jsx
@@ -1,7 +1,58 @@
-import React from "react";
-
 function Signup() {
-  return <div className="text-2xl font-bold flex justify-center">Signup</div>;
+  return (
+    <div className="h-[100vh] flex justify-center items-center">
+      <div className="shadow-md rounded-2xl border border-red-400 w-[20vw] h-[55vh]">
+        <div className="flex justify-center mt-3 font-bold">
+          <h3 className="px-10 font-bold text-2xl text-red-500">Register</h3>
+        </div>
+        <div className="mt-10 px-3">
+          <input
+            placeholder="Full name"
+            className="border outline-0 pl-3 rounded-md w-full py-2"
+            type="text"
+          />
+        </div>
+        <div className="mt-3 px-3">
+          <input
+            placeholder="Mother name"
+            className="border outline-0 pl-3 rounded-md w-full py-2"
+            type="text"
+          />
+        </div>
+        <div className="mt-3 px-3">
+          <input
+            placeholder="Father name"
+            className="border outline-0 pl-3 rounded-md w-full py-2"
+            type="text"
+          />
+        </div>
+        <div className="mt-3 px-3">
+          <input
+            placeholder="DOB"
+            className="border outline-0 pl-3 rounded-md w-full py-2"
+            type="text"
+          />
+        </div>
+        <div className="mt-3 px-3">
+          <input
+            placeholder="Email"
+            className="border outline-0 pl-3 rounded-md w-full py-2"
+            type="text"
+          />
+        </div>
+        <div className="mt-3 px-3">
+          <input
+            placeholder="Password"
+            className="border outline-0 pl-3 rounded-md w-full py-2"
+            type="text"
+          />
+        </div>
+        <div className="mt-5 px-3 flex justify-center">
+          <button className="bg-red-500 py-2 px-15 rounded-xl text-lg font-bold text-white hover:bg-red-600">Signup</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
