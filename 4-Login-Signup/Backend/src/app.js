d3fa2bd37d8ab7d8bb8a5c82156756463129f810
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-06-28 10:31:03 +0530

    Add CORS configuration and update environment variables for backend

--- 4-Login-Signup/Backend/src/app.js
+++ 4-Login-Signup/Backend/src/app.js
@@ -1,9 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Here write for connection with Frontend
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
 app.use(cookieParser());
 app.use(express.json());
