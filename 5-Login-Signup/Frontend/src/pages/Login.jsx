9d1375baa4e601f1b773e988934cca2545465b5e
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-06-30 15:31:49 +0530

    fix: initialize email and password state with empty strings in Login component

--- 5-Login-Signup/Frontend/src/pages/Login.jsx
+++ 5-Login-Signup/Frontend/src/pages/Login.jsx
@@ -3,8 +3,8 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const userLogin = (e) => {
