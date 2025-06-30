382597eab872354a392f2c9eed2cb9c0d56a6280
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-06-30 16:34:26 +0530

    implement Home component with user authentication and logout functionality and the Login-Signup-5 fully completed

--- 5-Login-Signup/Frontend/src/pages/Home.jsx
+++ 5-Login-Signup/Frontend/src/pages/Home.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userData = localStorage.getItem("user");
+    if (userData && userData !== "undefined") {
+      try {
+        setUser(JSON.parse(userData));
+      } catch (error) {
+        console.error("Faild to parse user data");
+        localStorage.removeItem("user");
+      }
+    }
+  }, []);
+
+  const userLogout = (e) => {
+    axios
+      .post("http://localhost:4000/api/v1/users/logout")
+      .then((res) => {
+        localStorage.removeItem("user");
+        setUser(null);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log("Logout error 😒", err);
+        alert("Somthing went wrong for logout 😒");
+      });
+  };
+
+  return (
+    <div className="h-[100vh] bg-green-300 flex flex-col justify-center items-center">
+      <div className="text-2xl text-yellow-700 font-bold tracking-wider">
+        Welcome {user?.fullName || "User"} 😍
+      </div>
+      <div className="mt-[20px]">
+        <button
+          className="bg-red-500 hover:bg-red-600 px-[25px] py-[7px] rounded-lg text-white font-semibold tracking-wider"
+          onClick={userLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
