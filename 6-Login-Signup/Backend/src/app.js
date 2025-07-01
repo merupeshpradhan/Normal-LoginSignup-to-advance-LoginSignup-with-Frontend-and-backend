4a2a0ec60187fbb9d2a76177c9e3c79cff4c1fbe
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-07-01 08:31:33 +0530

    fix: configure dotenv and update CORS settings in app.js

--- 6-Login-Signup/Backend/src/app.js
+++ 6-Login-Signup/Backend/src/app.js
@@ -1,9 +1,14 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+import express, { response } from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Frontend Connection here
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
 app.use(cookieParser());
 app.use(express.json());
