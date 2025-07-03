b0e637911971e27957660e897b61ffae6dc97b25
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-07-03 11:24:21 +0530

    Home page setup complete

--- 7-Login-Signup/Frontend/src/pages/Home.jsx
+++ 7-Login-Signup/Frontend/src/pages/Home.jsx
@@ -1,6 +1,65 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return <div>Home</div>;
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userData = localStorage.getItem("user");
+    if (userData && userData !== "undefined") {
+      try {
+        setUser(JSON.parse(userData));
+      } catch (error) {
+        console.log("Somthing went to wrong to get user data");
+        localStorage.removeItem("user");
+      }
+    }
+  }, []);
+
+  const userLogout = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:4000/api/v1/users/logout")
+      .then((res) => {
+        console.log("User you logout succefully! 🙄", res);
+        alert("User you logout succefully! 🙄");
+
+        setUser(null);
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(
+          "Somthing went wrong to logout 😒",
+          err.response?.data?.message || err.message
+        );
+        alert("Somthing went wrong setup you that why its not logout 😒");
+      });
+  };
+
+  return (
+    <div className="bg-green-200 h-[100vh] flex flex-col justify-center items-center">
+      <div className="text-xl font-semibold tracking-wider text-yellow-800 flex justify-center items-center">
+        <div className="flex flex-col">
+          <img
+            src={user?.avatar}
+            className="h-[20vh] border border-black w-[30vh] rounded-2xl"
+          />
+          <p className="mt-3">Welcome {user?.fullName || "User"} 😍</p>
+        </div>
+      </div>
+      <div className="mt-4">
+        <button
+          className="text-[15px] font-medium text-white tracking-wider bg-red-500 w-[100px] h-[35px] rounded-md"
+          onClick={userLogout}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
