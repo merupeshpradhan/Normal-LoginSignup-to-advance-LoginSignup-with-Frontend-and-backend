b1c529ef1756e659e2d8b5e70578ceeb678b3d3e
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-07-03 19:50:23 +0530

    Complete login functionality with form handling and user authentication

--- 7-Login-Signup/Frontend/src/pages/Login.jsx
+++ 7-Login-Signup/Frontend/src/pages/Login.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate("");
+
+  const userLogin = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:4000/api/v1/users/login", { email, password })
+      .then((res) => {
+        console.log("User login successfully 😍", res.data);
+
+        alert("Wellcome greate user for login this side 😍");
+        navigate("/home");
+
+        setEmail("");
+        setPassword("");
+      })
+      .catch((err) => {
+        console.error("User did not give correct email and password 🙄", err);
+        alert("Please provide correct email and password 🙄");
+      });
+  };
+
+  return (
+    <div className="bg-green-50 h-[100vh] flex justify-center items-center">
+      <div className="border border-green-300 rounded-md p-2 shadow-xl shadow-green-400 w-[70%] md:w-[45%] lg:w-[35%] xl:w-[20%] pb-5">
+        <div className="flex justify-center">
+          <h3 className="md:text-xl text-base font-semibold italic tracking-wider text-blue-600">
+            Login
+          </h3>
+        </div>
+        <form onSubmit={userLogin}>
+          <div className="mt-[24px] md:mt-[20px] flex flex-col items-center">
+            <input
+              className="border rounded-sm pl-2 text-sm xl:text-base py-[5px] w-[85%] h-[33px] md:h-[37px] xl:h-[45px] font-semibold tracking-widest focus:outline-green-400"
+              placeholder="Email"
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <input
+              className="border rounded-sm pl-2 text-sm xl:text-base py-[5px] w-[85%] h-[33px] md:h-[37px] xl:h-[45px] font-semibold tracking-widest mt-[12px] focus:outline-green-400"
+              placeholder="Email"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <button className="bg-green-500 hover:bg-green-600 mt-5 w-[45%] h-[32px] md:h-[37px] xl:h-[42px] rounded-lg font-semibold tracking-wider text-white xl:text-lg text-sm md:text-[15px]">
+              Login
+            </button>
+          </div>
+        </form>
+        <hr className="mt-5 w-full" />
+        <div className="flex justify-center">
+          <Link
+            to="/signup"
+            className="flex justify-center items-center bg-red-500 hover:bg-red-600 mt-5 w-[93%] h-[32px] md:h-[37px] xl:h-[42px] rounded-lg font-semibold tracking-wider text-white xl:text-lg text-sm md:text-[15px]"
+          >
+            Signup
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
