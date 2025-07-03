9c9529658598d39b7b6463bffe994440a817a0a3
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-07-03 09:48:30 +0530

    Fit all starting things

--- 7-Login-Signup/Frontend/src/App.jsx
+++ 7-Login-Signup/Frontend/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
